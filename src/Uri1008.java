8cf02b0488ecb76db82cbfdbe03a8cfe5b8d8bb3
Fernando Chagas <xchagasx@yahoo.com.br>
2024-08-27 20:41:01 -0300

    resolved uri1008

---
+++ src/Uri1008.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1008 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int func = sc.nextInt();
+        int hoursWork = sc.nextInt();
+        double hoursTime = sc.nextDouble();
+
+        double calcWork = hoursWork * hoursTime;
+
+        System.out.println("NUMBER = " + func);
+        System.out.printf("SALARY = U$ %.2f%n", calcWork);
+
+        sc.close();
+    }
+}
