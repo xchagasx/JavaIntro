8ea9a16dfa9e80a8c1067c6d8772ac8ec50318bd
Fernando Chagas <xchagasx@yahoo.com.br>
2024-08-26 21:58:07 -0300

    resolved uri1004

---
+++ src/Uri1004.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1004 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int mult = a * b;
+
+        System.out.println("PROD = " + mult);
+
+        sc.close();
+    }
+}
