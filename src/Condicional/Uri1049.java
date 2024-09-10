5820086af8b9e9a5ab7ff317a819084f6e8d2625
Fernando Chagas <xchagasx@yahoo.com.br>
2024-09-10 06:39:50 -0300

    fix and resolved uri 1049

--- src/Condicional/Uri1049.java
+++ src/Condicional/Uri1049.java
@@ -8,24 +8,46 @@ public class Uri1049 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        String p1;
-//        String p2;
-//        String p3;
-        p1 = sc.nextLine();
-//        p2 = sc.next();
-//        p3 = sc.next();
-        String vert = "vertebrado";
+        String palavra1 = sc.nextLine();
+        String palavra2 = sc.nextLine();
+        String palavra3 = sc.nextLine();
 
-        if (vert == p1) {
-            System.out.println("homem");
-        } else {
-            System.out.println("mulher");
+        if (palavra1.equals("vertebrado")) {
+            if (palavra2.equals("ave")) {
+                if (palavra3.equals("carnivoro")) {
+                    System.out.println("aguia");
+                }
+                else {
+                    System.out.println("pomba");
+                }
+            }
+            else {
+                if (palavra3.equals("onivoro")) {
+                    System.out.println("homem");
+                }
+                else {
+                    System.out.println("vaca");
+                }
+            }
         }
-//            if(p2 == "mamifero") {
-//                if (p3 == "onivoro") {
-//                }
-//            }
-//        }
+        else {
+            if (palavra2.equals("inseto")) {
+                if (palavra3.equals("hematofago")) {
+                    System.out.println("pulga");
+                }
+                else {
+                    System.out.println("lagarta");
+                }
+            }
+            else {
+                if (palavra3.equals("hematofago")) {
+                    System.out.println("sanguessuga");
+                }
+                else {
+                    System.out.println("minhoca");
+                }
+            }
+        }
 
         sc.close();
     }
