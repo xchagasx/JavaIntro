a321252676fb8950407d549bda13301e5e00936c
Fernando Chagas <xchagasx@yahoo.com.br>
2024-09-27 07:18:43 -0300

    exemploFor

---
+++ src/Repetitivas/For/ExemploFor2.java
@@ -0,0 +1,13 @@
+package Repetitivas.For;
+
+public class ExemploFor2 {
+    public static void main(String[] args) {
+//        for (int i = 0; i < 5; i++) { // incremento
+//            System.out.println("O valor de i: " + i);
+//        }
+
+        for (int i = 5; i >= 0 ; i--) { // decremento
+            System.out.println("O valor de i: " + i);
+        }
+    }
+}
