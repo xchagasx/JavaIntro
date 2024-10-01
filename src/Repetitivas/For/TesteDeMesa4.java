312285e96e50b844c927acb8d718b4a640db208d
Fernando Chagas <xchagasx@yahoo.com.br>
2024-10-01 05:30:23 -0300

    finish test of tables

---
+++ src/Repetitivas/For/TesteDeMesa4.java
@@ -0,0 +1,14 @@
+package Repetitivas.For;
+
+public class TesteDeMesa4 {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 0;
+
+        for (int i = 0; i < x; i++) {
+            System.out.print(i);
+            System.out.println(x);
+            y = y + 10;
+        }
+    }
+}
---
+++ src/Repetitivas/For/TesteDeMesa6.java
@@ -0,0 +1,14 @@
+package Repetitivas.For;
+
+public class TesteDeMesa6 {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 3;
+
+        for (int i = 0; y < x; i++) {
+            x = x - 2;
+            y = y + 1;
+            System.out.println(i);
+        }
+    }
+}
