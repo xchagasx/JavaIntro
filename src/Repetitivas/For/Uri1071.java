27a1fa6f1013e9525e785d2dfe41dd2b4ad44a20
Fernando Chagas <xchagasx@yahoo.com.br>
2024-10-10 07:18:48 -0300

    resolving uri1071

---
+++ src/Repetitivas/For/Uri1071.java
@@ -0,0 +1,16 @@
+package Repetitivas.For;
+
+import java.util.Scanner;
+
+public class Uri1071 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+         
+
+        sc.close();
+    }
+}
