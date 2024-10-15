409c0a36ac5d77a4ba5c484aca963fc1d375a261
Fernando Chagas <xchagasx@yahoo.com.br>
2024-10-15 07:29:02 -0300

    resolving uri1071

--- src/Repetitivas/For/Uri1071.java
+++ src/Repetitivas/For/Uri1071.java
@@ -8,8 +8,24 @@ public class Uri1071 {
 
         int X = sc.nextInt();
         int Y = sc.nextInt();
+        int min, max;
 
-         
+        if (X < Y) {
+            min = X;
+            max = Y;
+        } else {
+            min = Y;
+            max = X;
+        }
+
+        int soma = 0;
+        for (int i = min + 1; i < max; i++) {
+            if (i % 2 != 0) {
+                soma = soma + 1;
+            }
+        }
+
+        System.out.println(soma);
 
         sc.close();
     }
