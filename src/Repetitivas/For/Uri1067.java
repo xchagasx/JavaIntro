9d89a6b4f8f44b452a67566ffb63b2e4ef3cad3b
Fernando Chagas <xchagasx@yahoo.com.br>
2024-10-17 07:21:22 -0300

    resolving Uri1067

---
+++ src/Repetitivas/For/Uri1067.java
@@ -0,0 +1,19 @@
+package Repetitivas.For;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1067 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        for (int i = 0; i < x; i++) {
+            
+        }
+
+        sc.close();
+    }
+}
