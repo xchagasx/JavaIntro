690e0ccffde5e11d8a21532e7fda59046c4d6683
Fernando Chagas <xchagasx@yahoo.com.br>
2024-10-23 07:27:25 -0300

    resolving uri1067

--- src/Repetitivas/For/Uri1067.java
+++ src/Repetitivas/For/Uri1067.java
@@ -9,16 +9,19 @@ public class Uri1067 {
         Scanner sc = new Scanner(System.in);
 
         int x = sc.nextInt();
-        int number;
-        for (int i = 0; i < x; i++) {
-            if (x % 2 == 0) { // CASO O NÚMERO SEJA PAR
-                number = x - (x - 1); // SUBTRAIR POR 1
-                System.out.println(number);
-            } else if (x % 2 != 0) { // CASO O NÚMERO SEJA ÍMPAR
-                number = x; // MANTENHA
-                System.out.println(number);
-            }
+        int number = 0;
+//
+        if (x % 2 == 0) { // CASO O NÚMERO SEJA PAR
+            number = x - (x - 1); // SUBTRAIR POR 1
+            System.out.println(number);
+        } else if (x % 2 != 0) { // CASO O NÚMERO SEJA ÍMPAR
+            number = x - (x - 1); // MANTENHA
+            System.out.println(number);
         }
+        for (int i = 0; i > number; i++) {
+            System.out.println(number + 2);
+        }
+//        int sum = 2;
         sc.close();
     }
 }
