4a09aa9bcf94f67d2c15fbbd87c81bb422512df3
Fernando Chagas <xchagasx@yahoo.com.br>
2024-10-30 07:16:17 -0300

    resolved uri1074

--- src/Repetitivas/For/Uri1074.java
+++ src/Repetitivas/For/Uri1074.java
@@ -7,8 +7,27 @@ public class Uri1074 {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        
 
+        for (int i=0; i<n; i++) {
+
+            int x = sc.nextInt();
+
+            if (x == 0) {
+                System.out.println("NULL");
+            }
+            else if (x % 2 == 0 && x < 0) {
+                System.out.println("EVEN NEGATIVE");
+            }
+            else if (x % 2 == 0 && x > 0) {
+                System.out.println("EVEN POSITIVE");
+            }
+            else if (x % 2 != 0 && x > 0) {
+                System.out.println("ODD POSITIVE");
+            }
+            else {
+                System.out.println("ODD NEGATIVE");
+            }
+        }
 
         sc.close();
     }
