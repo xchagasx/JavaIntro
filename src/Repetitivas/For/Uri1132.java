1a77a320989bb1f85f4594836be3aefd3141e603
Fernando Chagas <xchagasx@yahoo.com.br>
2025-01-03 06:48:00 -0300

    resolving uri1132

--- src/Repetitivas/For/Uri1132.java
+++ src/Repetitivas/For/Uri1132.java
@@ -10,12 +10,27 @@ public class Uri1132 {
 
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
-
-        for (int i = 0; i < n1; i++) {
-            if (n1 > n2 || n2 > n1) {
-                System.out.println("Hello Guyz");
-            }
+//        int calc =  n1 - n2;
+        if (n1 > n2) {
+            int calc1 =  n1 - n2;
+                for (int i = 0; i < calc1; i = i + calc1) {
+                    if (calc1 % 13 != 0) {
+                        calc1 = calc1 + calc1;
+                    }
+                }
+            System.out.println(calc1);
         }
+//        System.out.println(calc1);
+//        if (n1 > n2) {
+//            int calc1 =  n1 - n2;
+//            System.out.println(calc1);
+//            if (calc1 % 13 != 0) {
+//
+//            }
+//        } else {
+//            int calc1 = n2 - n1;
+//            System.out.println(calc1);
+//        }
 
         sc.close();
     }
