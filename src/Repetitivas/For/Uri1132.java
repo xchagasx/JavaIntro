d799f06bfb5de08f87a6ff0f76aea6a76f28565e
Fernando Chagas <xchagasx@yahoo.com.br>
2025-01-04 00:12:56 -0300

    resolved uri1132

--- src/Repetitivas/For/Uri1132.java
+++ src/Repetitivas/For/Uri1132.java
@@ -8,30 +8,25 @@ public class Uri1132 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-//        int calc =  n1 - n2;
-        if (n1 > n2) {
-            int calc1 =  n1 - n2;
-                for (int i = 0; i < calc1; i = i + calc1) {
-                    if (calc1 % 13 != 0) {
-                        calc1 = calc1 + calc1;
-                    }
-                }
-            System.out.println(calc1);
-        }
-//        System.out.println(calc1);
-//        if (n1 > n2) {
-//            int calc1 =  n1 - n2;
-//            System.out.println(calc1);
-//            if (calc1 % 13 != 0) {
-//
-//            }
-//        } else {
-//            int calc1 = n2 - n1;
-//            System.out.println(calc1);
-//        }
+        int x = sc.nextInt();
+        int y = sc.nextInt();
 
+        int menor = x;
+        int maior = y;
+
+        if (x > y) {
+            menor = y;
+            maior = x;
+        }
+
+        int soma = 0;
+        for (int i = menor; i <= maior; i++) {
+            if (i % 13 != 0) {
+                soma = soma + i;
+            }
+        }
+
+        System.out.println(soma);
         sc.close();
     }
 }
