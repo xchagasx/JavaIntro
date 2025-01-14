365bc0fa85e095754e6cce066e8dcaa0eab7e52a
Fernando Chagas <xchagasx@yahoo.com.br>
2025-01-14 07:11:12 -0300

    resolved uri1094

--- src/Repetitivas/For/Uri1094.java
+++ src/Repetitivas/For/Uri1094.java
@@ -6,7 +6,10 @@ public class Uri1094 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
+        int numberAnimal = sc.nextInt();
+        String letraAnimal = sc.nextLine();
+
+        System.out.println(numberAnimal + " " + letraAnimal);
 
         sc.close();
     }
