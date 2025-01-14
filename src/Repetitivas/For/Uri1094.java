ffbc5a3da0b0fb3041ab8bdc425312231eec44ab
Fernando Chagas <xchagasx@yahoo.com.br>
2025-01-14 07:06:15 -0300

    resolved uri180

---
+++ src/Repetitivas/For/Uri1094.java
@@ -0,0 +1,13 @@
+package Repetitivas.For;
+
+import java.util.Scanner;
+
+public class Uri1094 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+    }
+}
