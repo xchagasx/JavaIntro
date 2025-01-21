cdec000314db0099816b757e2d5d0be60c76cbb8
Fernando Chagas <xchagasx@yahoo.com.br>
2025-01-21 07:17:37 -0300

    resolving uri1158

---
+++ src/Repetitivas/For/Uri1158.java
@@ -0,0 +1,17 @@
+package Repetitivas.For;
+
+import java.util.Scanner;
+
+public class Uri1158 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1, n2;
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+
+        
+
+        sc.close();
+    }
+}
